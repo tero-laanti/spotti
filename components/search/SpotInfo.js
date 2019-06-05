3d86e61df4681d660d76ba7d4ab315e9a850c71e
tero-yrjola <yrjola.tero@gmail.com>
2019-06-05 22:25:26 +0300

    Add the spotinfopage skeleton

--- components/search/SpotInfo.js
+++ components/search/SpotInfo.js
@@ -1,15 +1,45 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { View, Button } from 'react-native';
+import { View, Button, Text, ScrollView } from 'react-native';
 
 const SpotInfo = ({ navigation }) => (
-  <View>
-    <Button title="Spot this" onPress={() => navigation.navigate('Purchase')} />
+  <View
+    style={{
+      display: 'flex',
+      height: '100%',
+      flexDirection: 'column',
+      paddingHorizontal: 10,
+      paddingTop: 20,
+    }}
+  >
+    <View style={{ paddingBottom: 10, flex: 1 }}>
+      <View style={{ backgroundColor: 'blue', height: 150, width: 300, margin: 30 }}>
+        <Text>Image goes here</Text>
+      </View>
+      <Text style={{ paddingBottom: 10, fontWeight: 'bold' }}>
+        Distance to the spot: {navigation.state.params.spot.distance}
+      </Text>
+      <ScrollView>
+        <Text>{navigation.state.params.spot.description}</Text>
+      </ScrollView>
+    </View>
+    <View style={{ flex: 0 }}>
+      <Button title="Spot this" onPress={() => navigation.navigate('Purchase')} />
+    </View>
   </View>
 );
 
 SpotInfo.propTypes = {
-  navigation: PropTypes.shape({}).isRequired,
+  navigation: PropTypes.shape({
+    state: PropTypes.shape({
+      params: PropTypes.shape({
+        spot: PropTypes.shape({
+          distance: PropTypes.string.isRequired,
+          description: PropTypes.string.isRequired,
+        }),
+      }),
+    }),
+  }).isRequired,
 };
 
 export default SpotInfo;
