ce819698bbc3613ec3d7a39d5ceb8aa191a4dfa3
tero-yrjola <yrjola.tero@gmail.com>
2019-06-16 16:31:22 +0300

    Add a imagecarousel component to spotinfopage that currently just displays the imageUrls of the spot

--- components/spot-info/SpotInfo.js
+++ components/spot-info/SpotInfo.js
@@ -1,33 +1,64 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { View, Button, Text, ScrollView } from 'react-native';
+import Carousel from 'react-native-snap-carousel';
 
-const SpotInfo = ({ navigation }) => (
-  <View
-    style={{
-      display: 'flex',
-      height: '100%',
-      flexDirection: 'column',
-      paddingHorizontal: 10,
-      paddingTop: 20,
-    }}
-  >
-    <View style={{ paddingBottom: 10, flex: 1 }}>
-      <View style={{ backgroundColor: 'blue', height: 150, width: 300, margin: 30 }}>
-        <Text>Image goes here</Text>
-      </View>
-      <Text style={{ paddingBottom: 10, fontWeight: 'bold' }}>
-        Distance to the spot: {navigation.state.params.spot.distance}
-      </Text>
-      <ScrollView>
-        <Text>{navigation.state.params.spot.description}</Text>
-      </ScrollView>
-    </View>
-    <View style={{ flex: 0 }}>
-      <Button title="Spot this" onPress={() => navigation.navigate('Purchase')} />
-    </View>
+const fakeImageFetchByUrl = url => (
+  <View style={{ backgroundColor: 'blue', height: 150, width: 300, margin: 30 }}>
+    <Text>{url}</Text>
   </View>
 );
+
+const SpotInfo = ({
+  navigation: {
+    state: {
+      params: { spot },
+    },
+  },
+  navigation,
+}) => {
+  const renderImageFromUrl = carouselItem => fakeImageFetchByUrl(carouselItem.item);
+
+  return (
+    <View
+      style={{
+        display: 'flex',
+        height: '100%',
+        flexDirection: 'column',
+        paddingHorizontal: 10,
+        paddingTop: 20,
+      }}
+    >
+      <View style={{ paddingBottom: 10, flex: 1 }}>
+        {spot.urls.length > 0 ? (
+          <View style={{ height: 175 }}>
+            <Carousel
+              activeSlideAlignment="start"
+              data={spot.urls}
+              renderItem={renderImageFromUrl}
+              sliderWidth={450}
+              itemWidth={300}
+              sliderHeight={50}
+            />
+          </View>
+        ) : (
+          <View style={{ alignItems: 'center', paddingVertical: 30 }}>
+            <Text>No images available!</Text>
+          </View>
+        )}
+        <Text style={{ paddingBottom: 10, fontWeight: 'bold' }}>
+          Distance to the spot: {spot.distance}
+        </Text>
+        <ScrollView>
+          <Text>{spot.description}</Text>
+        </ScrollView>
+      </View>
+      <View style={{ flex: 0 }}>
+        <Button title="Spot this" onPress={() => navigation.navigate('Purchase')} />
+      </View>
+    </View>
+  );
+};
 
 SpotInfo.propTypes = {
   navigation: PropTypes.shape({
@@ -36,6 +67,7 @@ SpotInfo.propTypes = {
         spot: PropTypes.shape({
           distance: PropTypes.string.isRequired,
           description: PropTypes.string.isRequired,
+          imageUrls: PropTypes.arrayOf(PropTypes.string),
         }).isRequired,
       }),
     }),
