d637ae5dd778c689e8a9b82f5aa90768d35e4b71
Joona Juusti <joona.juusti@gmail.com>
2019-06-29 12:33:31 +0300

    Add autocomplete suggestions and search coordinates as mock data

---
+++ components/mock-data.js
@@ -0,0 +1,28 @@
+export const AUTOCOMPLETE_SUGGESTIONS = {
+  kup: [
+    'Kupari, Croatia',
+    'Kupjak, Croatia',
+    'Kupang, Indonesia',
+    'Kupa',
+    'Kupres, Bosnia and Herzegovina',
+  ],
+  kupi: [
+    'Brod Na Kupi, Croatia',
+    'Kupiskis, Lithuania',
+    'Kupittaa, Turku',
+    'Kupittaankatu, Turku',
+    'Kupinovo, Serbia',
+  ],
+  kupit: [
+    'Kupittaa, Turku',
+    'Kupittaankatu, Turku',
+    'Kupittaanpuisto, Turku',
+    'Kupittaan asema, Turku',
+    'Kupittaan urheiluhalli, Turku',
+  ],
+};
+
+export const SEARCH_COORDINATES = {
+  latitude: 60.45,
+  longitude: 22.29,
+};
