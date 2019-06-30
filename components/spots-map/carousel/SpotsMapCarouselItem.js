fb40d52f4ee7e1e83ac5113295c5a0d407e29011
tero-yrjola <yrjola.tero@gmail.com>
2019-06-30 19:25:37 +0300

    Add distance and price to carousel item

--- components/spots-map/carousel/SpotsMapCarouselItem.js
+++ components/spots-map/carousel/SpotsMapCarouselItem.js
@@ -1,20 +1,38 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { StyleSheet, Text, TouchableHighlight, View } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 const styles = StyleSheet.create({
   carouselItem: {
     width: 250,
-    backgroundColor: 'blue',
+    marginVertical: 5,
+    backgroundColor: 'lightsteelblue',
+    borderRadius: 3,
+    elevation: 4,
+    shadowOpacity: 0.75,
+    shadowRadius: 5,
+    shadowOffset: { height: 5, width: 5 },
   },
 });
 
 const SpotsMapCarouselItem = ({ navigation, spot }) => (
-  <TouchableOpacity onPress={() => navigation.navigate('SpotInfo', { spot })}>
+  <TouchableHighlight onPress={() => navigation.navigate('SpotInfo', { spot })}>
     <View style={styles.carouselItem}>
-      <Text>{`Title: ${spot.address}`}</Text>
+      <View>
+        <Text style={{ textAlign: 'center', fontSize: 18 }}>{spot.address}</Text>
+      </View>
+      <View style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
+        <View>
+          <Text style={{ fontSize: 16 }}>
+            <Icon name="directions-walk" />
+            {spot.distance || 'N/A'}
+          </Text>
+        </View>
+        <Text style={{ fontSize: 16 }}>3€ / h</Text>
+      </View>
     </View>
-  </TouchableOpacity>
+  </TouchableHighlight>
 );
 
 SpotsMapCarouselItem.propTypes = {
