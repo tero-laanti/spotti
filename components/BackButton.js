d45458f516f0840532da5d66bb4e531832530820
tero-yrjola <yrjola.tero@gmail.com>
2019-07-13 09:43:36 +0300

    Remove unused stuff in BackButton

--- components/BackButton.js
+++ components/BackButton.js
@@ -1,19 +1,12 @@
 import React from 'react';
-import { StyleSheet, TouchableWithoutFeedback, View } from 'react-native';
+import { TouchableWithoutFeedback, View } from 'react-native';
 import PropTypes from 'prop-types';
 import Icon from 'react-native-vector-icons/FontAwesome';
-
-const styles = StyleSheet.create({
-  separator: {
-    borderBottomColor: '#B4B4B4',
-    borderBottomWidth: 1,
-  },
-});
 
 const BackButton = ({ onPress }) => (
   <TouchableWithoutFeedback onPress={onPress}>
     <View>
-      <Icon name="arrow-left" size={30} color="#B4B4B4" style={styles.icon} />
+      <Icon name="arrow-left" size={30} color="#B4B4B4"/>
     </View>
   </TouchableWithoutFeedback>
 );
