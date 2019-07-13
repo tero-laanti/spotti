f921fca8db2063762d788c7ec5d24b76b00a75e8
Joona Juusti <joona.juusti@gmail.com>
2019-07-13 13:15:59 +0300

    Make address and description editable

--- components/own-listings/EditListingScreen.js
+++ components/own-listings/EditListingScreen.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
-import { View, StyleSheet, TouchableOpacity, Text, Button } from 'react-native';
+import { View, StyleSheet, TouchableOpacity, Text, Button, TextInput } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
 const styles = StyleSheet.create({
@@ -18,12 +18,18 @@ const styles = StyleSheet.create({
   infoHeader: {
     flexDirection: 'row',
     justifyContent: 'space-between',
+    alignContent: 'center',
   },
   spotInfoSection: {
     paddingTop: 10,
     paddingBottom: 10,
     borderBottomColor: '#B4B4B4',
     borderBottomWidth: 1,
+  },
+  textInput: {
+    paddingTop: 0,
+    paddingBottom: 0,
+    maxHeight: 100,
   },
 });
 
@@ -34,33 +40,65 @@ const EditListingScreen = ({
       params: { spot },
     },
   },
-}) => (
-  <View style={styles.container}>
-    <View>
-      <TouchableOpacity onPress={() => navigation.goBack()} style={styles.iconButton}>
-        <View>
-          <Icon name="arrow-left" size={30} color="#B4B4B4" style={styles.icon} />
+}) => {
+  const [address, setAddress] = useState('');
+  const [description, setDescription] = useState('');
+  const [isDescriptionFocused, setIsDescriptionFocused] = useState(false);
+
+  useEffect(() => {
+    setAddress(spot.address);
+    setDescription(spot.description);
+  }, [spot]);
+
+  const getDescriptionInputValue = () =>
+    isDescriptionFocused ? description : description || 'No description yet';
+
+  return (
+    <View style={styles.container}>
+      <View>
+        <TouchableOpacity onPress={() => navigation.goBack()} style={styles.iconButton}>
+          <View>
+            <Icon name="arrow-left" size={30} color="#B4B4B4" style={styles.icon} />
+          </View>
+        </TouchableOpacity>
+        <View style={[styles.infoHeader, styles.spotInfoSection]}>
+          <TextInput
+            value={address}
+            onChangeText={text => setAddress(text)}
+            style={styles.textInput}
+          />
+          <Text style={{ textAlignVertical: 'center' }}>{`${spot.price}€/h`}</Text>
         </View>
-      </TouchableOpacity>
-      <View style={[styles.infoHeader, styles.spotInfoSection]}>
-        <Text>{spot.address}</Text>
-        <Text>{`${spot.price}€/h`}</Text>
+        <View style={styles.spotInfoSection}>
+          <Text>Available times:</Text>
+          <Button title="Edit times"></Button>
+        </View>
+        <View style={styles.spotInfoSection}>
+          <Text>Description:</Text>
+          <TextInput
+            value={getDescriptionInputValue()}
+            onChangeText={text => setDescription(text)}
+            onFocus={() => setIsDescriptionFocused(true)}
+            onBlur={() => setIsDescriptionFocused(false)}
+            blurOnSubmit
+            multiline
+            style={styles.textInput}
+          />
+        </View>
       </View>
-      <View style={[styles.availableTimesContainer, styles.spotInfoSection]}>
-        <Text>Available times:</Text>
-        <Button title="Edit times" size={100}></Button>
-      </View>
-      <View style={[styles.description, styles.spotInfoSection]}>
-        <Text>{spot.description || 'No description available.'}</Text>
-      </View>
+      {!isDescriptionFocused && <Button title="Save"></Button>}
     </View>
-    <Button title="Save"></Button>
-  </View>
-);
+  );
+};
 
 EditListingScreen.propTypes = {
-  navigation: PropTypes.shape({}).isRequired,
-  spot: PropTypes.shape({}).isRequired,
+  navigation: PropTypes.shape({
+    state: PropTypes.shape({
+      params: PropTypes.shape({
+        spot: PropTypes.shape({}).isRequired,
+      }),
+    }),
+  }).isRequired,
 };
 
 export default EditListingScreen;
