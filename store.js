72f1c70910c5d956dac9a1cb183cef8482015c1b
Joona Juusti <joona.juusti@gmail.com>
2019-08-07 18:58:29 +0300

    Fix linter error

--- store.js
+++ store.js
@@ -6,8 +6,6 @@ import rootReducer from './rootReducer';
 
 const middleware = [thunk];
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const initStore = () => {
-  return createStore(rootReducer, composeEnhancers(applyMiddleware(...middleware)));
-};
+const initStore = () => createStore(rootReducer, composeEnhancers(applyMiddleware(...middleware)));
 
 export default initStore;
