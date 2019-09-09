f5d9a69725bb6ba40763cb4fd1bd6e03e1a0a8df
tero-yrjola <yrjola.tero@gmail.com>
2019-09-09 08:48:06 +0300

    Improve search by current location

--- src/containers/reservation/FrontPage.js
+++ src/containers/reservation/FrontPage.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import {
   View,
@@ -10,12 +10,14 @@ import {
 } from 'react-native';
 import AwesomeIcon from 'react-native-vector-icons/FontAwesome';
 import MaterialIcon from 'react-native-vector-icons/MaterialIcons';
-import SpottiText from '../../static/spotti-logotype-blue.png';
-import SpottiLogo from '../../static/spotti-logo-blue.png';
+import SpottiText from '../../static/spotti-logotype-white.png';
+import SpottiLogo from '../../static/spotti-logo-white.png';
 import routes from './routes';
+import colors from '../../Theme';
 
 const styles = StyleSheet.create({
-  button: {
+  searchButton: {
+    backgroundColor: 'snow',
     width: '70%',
     borderColor: 'black',
     borderWidth: 1,
@@ -24,8 +26,16 @@ const styles = StyleSheet.create({
     padding: 10,
     flexDirection: 'row',
     marginBottom: 8,
+    borderRadius: 5,
+    elevation: 4,
+    shadowOpacity: 0.9,
+    shadowRadius: 10,
+    shadowOffset: { height: 15, width: 15 },
   },
   container: {
+    paddingTop: 100,
+    height: 300,
+    backgroundColor: colors.primary,
     flex: 1,
     flexDirection: 'column',
     justifyContent: 'center',
@@ -33,14 +43,16 @@ const styles = StyleSheet.create({
   },
   logoContainer: {
     width: '100%',
-    height: '30%',
+    height: '40%',
     position: 'absolute',
-    top: 10,
+    top: 70,
     flex: 1,
     justifyContent: 'space-around',
     alignItems: 'center',
   },
   searchIconContainer: {
+    justifyContent: 'center',
+    marginBottom: 2,
     width: 30,
   },
   searchTextContainer: {
@@ -63,29 +75,26 @@ const styles = StyleSheet.create({
     resizeMode: 'center',
   },
   useCurrentLocationContainer: {
-    flexDirection: 'row',
+    justifyContent: 'center',
+    backgroundColor: 'snow',
     paddingVertical: 8,
+    flexDirection: 'row',
     paddingHorizontal: 10,
-    borderBottomColor: '#B4B4B4',
-    borderBottomWidth: 1,
+    borderRadius: 5,
+    borderColor: 'black',
+    borderWidth: 1,
   },
 });
 
 const FrontPage = ({ navigation }) => {
-  const [isFetchingLocation, setIsFetchingLocation] = useState(false);
-
-  const searchWithCurrentLocation = () => {
-    setIsFetchingLocation(true);
+  const [isFetchingLocation, setIsFetchingLocation] = useState(true);
+  const [currentLocation, setCurrentLocation] = useState(null);
+  const [searchButtonClicked, setSearchButtonClicked] = useState(false);
+  useEffect(() => {
     navigator.geolocation.getCurrentPosition(
       location => {
         setIsFetchingLocation(false);
-        navigation.navigate(routes.spotsMap, {
-          searchCoordinates: {
-            latitude: location.coords.latitude,
-            longitude: location.coords.longitude,
-          },
-          disableSearchLocationMarker: true,
-        });
+        setCurrentLocation(location);
       },
       error => {
         alert(error.message.toString());
@@ -93,7 +102,21 @@ const FrontPage = ({ navigation }) => {
       },
       { timeout: 10000, maximumAge: 1000 }
     );
-  };
+  }, []);
+
+  useEffect(() => {
+    if (searchButtonClicked && currentLocation) {
+      setIsFetchingLocation(false);
+      setSearchButtonClicked(false);
+      navigation.navigate(routes.spotsMap, {
+        searchCoordinates: {
+          latitude: currentLocation.coords.latitude,
+          longitude: currentLocation.coords.longitude,
+        },
+        disableSearchLocationMarker: true,
+      });
+    } else setIsFetchingLocation(false);
+  }, [searchButtonClicked, currentLocation]);
 
   return (
     <View style={styles.container}>
@@ -102,22 +125,22 @@ const FrontPage = ({ navigation }) => {
         <Image style={styles.logoText} source={SpottiText} />
       </View>
       <TouchableWithoutFeedback onPress={() => navigation.navigate(routes.search)}>
-        <View style={styles.button}>
+        <View style={styles.searchButton}>
           <View style={styles.searchIconContainer}>
-            <AwesomeIcon name="search" size={30} color="#B4B4B4" />
+            <AwesomeIcon name="search" size={30} color={colors.dark} />
           </View>
           <View style={styles.searchTextContainer}>
-            <Text style={styles.searchText}>Search</Text>
+            <Text style={styles.searchText}>Search for spots</Text>
           </View>
         </View>
       </TouchableWithoutFeedback>
       {isFetchingLocation ? (
         <ActivityIndicator size="large" color="#0000ff" />
       ) : (
-        <TouchableWithoutFeedback onPress={() => searchWithCurrentLocation()}>
+        <TouchableWithoutFeedback onPress={() => setSearchButtonClicked(true)}>
           <View style={styles.useCurrentLocationContainer}>
-            <MaterialIcon name="my-location" size={20} color="#B4B4B4" />
-            <Text> Use my current location</Text>
+            <MaterialIcon name="my-location" size={20} color={colors.dark} />
+            <Text style={{ color: '#B4B4B4' }}> Use my current location</Text>
           </View>
         </TouchableWithoutFeedback>
       )}
